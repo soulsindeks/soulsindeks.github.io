5c62ab14b4dd170167f364fc244ecbb92c90abca
soulsindeks <soulsindeks@gmail.com>
2024-02-18 00:37:40 +0700

    fix: sidebar bug

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const hamburger = document.querySelector(".hamburger")
+let navbarAbout = document.querySelector(".navbar__about")
+let navbar = document.querySelector(".navbar")
+let navbarItem = document.querySelectorAll(".navbar__item")
+let links = document.querySelectorAll(".navbar__about a")
 let toTop = document.querySelector(".toTop");
 let rootElement = document.documentElement;
 
@@ -21,12 +26,13 @@ toTop.addEventListener("click", scrollToTop);
 document.addEventListener("scroll", handleScroll);
 
 
-const hamburger = document.querySelector(".hamburger")
-let navbarAbout = document.querySelector(".navbar__about")
-let navbar = document.querySelector(".navbar")
-
-
 hamburger.addEventListener("click", mobileMenu);
+document.onclick = function(e){
+    if (!hamburger.contains(e.target) && !navbarItem.contains(e.target)) {
+        navbarItem.classList.remove("active")
+        navbar.classList.remove("active")
+    }
+}
 
 function mobileMenu() {
     hamburger.classList.toggle("-menu-open")
@@ -44,5 +50,15 @@ function toggleMenu() {
     let body = document.body
     body.classList.toggle('body__noscroll')
 }
+
+links.forEach(function(link) {
+    link.addEventListener('click', function() {
+      if (navbarAbout.classList.contains("active")) {
+        navbarAbout.classList.remove("active")
+        hamburger.classList.remove("-menu-open")
+      }
+      sidebar.style.display = 'none';
+    });
+  });
 
 
